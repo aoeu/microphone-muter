5dc1d85203737d397bb029cdb1c80d732ff0e035
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2020-08-03 17:11:10 -0400

    Display a toast upon app start

--- java/microphone/muter/Main.java
+++ java/microphone/muter/Main.java
@@ -3,6 +3,7 @@ package microphone.muter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class Main extends Activity {
 
@@ -13,6 +14,7 @@ public class Main extends Activity {
 			stopService(i);
 		} else {
 			startService(i);
+			showToast();
 		}
 		finish();
 	}
@@ -20,5 +22,11 @@ public class Main extends Activity {
 	boolean shouldStopService() {
 		Bundle b = getIntent().getExtras();
 		return b != null && b.getBoolean(MicrophoneMuter.keyToStop, false);
+	}
+
+	void showToast() {
+		String s = "The microphone is currently disabled for all Apps"
+			+ " and may be re-enabled via the notification drawer.";
+		Toast.makeText(this, s, Toast.LENGTH_LONG).show();
 	}
 }
